8cb2a85ca487a7e6f85189fad31dbd15d6cea85e
Jiachen  Wu <wujiachen2016@gmail.com>
2025-03-31 23:16:34 +0100

    fix migration error when disabled space-age

--- migrations/2.2.2.lua
+++ migrations/2.2.2.lua
@@ -1,5 +1,5 @@
 for i, force in pairs(game.forces) do
-  if force.technologies["agricultural-science-pack"].researched then
+  if force.technologies["chemical-science-pack"].researched then
     force.recipes["refrigerater"].enabled = true
   else
     force.recipes["refrigerater"].enabled = false
